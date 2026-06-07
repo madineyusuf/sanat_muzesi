a4898ede66860c73c17bcae5290163c8bc25d996
Madina Yusupova <yusvpova@gmail.com>
2026-06-07 16:33:55 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,25 +1,12 @@
 <?php
-
-$is_localhost = in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) || $_SERVER['SERVER_NAME'] == 'localhost';
-
-$dbname = 'dbstorage24360859922'; 
-
-if ($is_localhost) {
-    $host = 'localhost';
-    $username = 'root'; 
-    $password = '';    
-} else {
-    $host = 'localhost';
-    $username = 'dbusr24360859922';
-    $password = 'SxH5hSixGy8h';
-}
+$host = 'localhost';
+$db   = 'dbstorage24360859922';
+$user = 'dbusr24360859922';
+$pass = 'SxH5hSixGy8h';
 
 try {
-    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Veritabanı bağlantı hatası: " . $e->getMessage());
 }
-?>
