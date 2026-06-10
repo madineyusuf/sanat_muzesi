f05c46eb293b1ff85ec218de36e5150970c669c3
Madina Yusupova <yusvpova@gmail.com>
2026-06-10 18:19:16 +0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer class="bg-dark text-light text-center py-3 mt-5">
-    <small>© 2026 Sanat Müzesi</small>
+    <small>© BM BTÜ 2026 </small>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
